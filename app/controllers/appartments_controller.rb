c4a2c9d6494849fd976d0244404dc5aa7cbea44a
munyite-megzs <margaret.sitati1@gmail.com>
2015-04-27 21:51:30 +0300

    modified the appartment and show controler

--- app/controllers/appartments_controller.rb
+++ app/controllers/appartments_controller.rb
@@ -1,24 +1,22 @@
 class AppartmentsController < ApplicationController
 	def new
 		
+		
 	end
 
 	def create
-		#render plain: params[:appartment].inspect
-		@appartment = Appartment.new(appartment_params)
-		
+  @appartment = Appartment.new(appartment_params)
+ 
+   @appartment.save
+  redirect_to @appartment
+end
 
-		@appartment.save
-		redirect_to @appartment
-	end
-
-	def show
-
-		@appartment = Appartment.find(params[:id])
-		
-	end
-
-	private
+def show
+	@appartment = Appartment.find(params[:id])
+	
+end
+ 
+private
   def appartment_params
     params.require(:appartment).permit(:title, :description)
   end
